920b774dbb02805d5e7115573fe191c62fbc3f75
Ofir408 <33639234+Ofir408@users.noreply.github.com>
2018-04-21 19:43:47 +0300

    Upload Python Machine Learning code

---
+++ Python - Machine Learning/boolean_conj_predictor.py
@@ -0,0 +1,113 @@
+
+# Name: Ofir Ben-Shoham
+# Id:   208642496
+
+import sys
+import numpy as np
+
+
+filePath = sys.argv[1]
+training_examples = np.loadtxt(filePath).tolist()
+
+# load x, y from the file.
+def loadDataFromFile():
+    Xexamples = []
+    YexamplesTags = []
+    numberOfVar = len(training_examples[0]) - 1 # decrease one (tag value). numberOfVar = 4 in our code.
+
+    # convert data from to Xexamples & YexamplesTags
+    for current in training_examples:
+
+        Xexamples.append(current[0:numberOfVar]) # update xExamples
+        YexamplesTags.append(current[-1])
+
+    # convert lists to integers.
+    index = 0
+    for currentExample in Xexamples:
+        other = [int(i) for i in currentExample]
+        Xexamples[index] = other
+        index += 1
+    YexamplesTags = [int(i) for i in YexamplesTags]
+
+    return Xexamples, YexamplesTags, numberOfVar
+
+
+    # for example if finalListResult = " [1, 2, -3], sizeOfLIst = 3,
+    #  it means that we should write x1,x2,not(x3) into our text
+def covertToText(finalListResult):
+
+    strToWrite = ""
+    for currentNum in finalListResult:
+        j = abs(currentNum)
+        if currentNum != j:
+            # negative number, write not(i)
+            strToWrite += "not(x" + str(j) + "),"
+        else:
+            strToWrite += "x" +(str(currentNum) + ",")
+
+    withOutlast = strToWrite[0:len(strToWrite) - 1]  # without last ","
+    with open("output.txt", "w") as f:
+        f.write(withOutlast)
+
+
+def ConsistencyAlgorithm(X, Y, d, numberOfExamples):
+    # initialize all_negative_hypothesis
+    h = []
+    for i in range(1, d + 1):
+        h.append(i)
+        h.append(-i) # since we want the negative of each element in all_negative_hypothesis.
+    hPrev = h
+    # finish to build all_negative
+
+    for instanceIndex in range(0, numberOfExamples):
+        h = hPrev
+        if Y[instanceIndex] == 1 and checkIfTrue(X[instanceIndex], h) == 0:
+            for index in range(0, len(X[instanceIndex])):
+                currentIndexVal = X[instanceIndex][index]
+
+                if currentIndexVal == 1:
+                    if (-(index+1)) in h:
+                        h.remove(-(index+1))
+                if currentIndexVal == 0:
+                    if (index+1) in h:
+                        h.remove(index + 1)
+
+        hPrev = h
+    return h
+
+    # write the result to text file
+
+
+
+# for example getting (-1, 2, 1) = not(x1) & x2 & x1.
+# This method replace the not to normal values, then checks if the final result is true or false.
+def checkIfTrue(xListFromFile, v):
+    zerosAndOnesList = [convertToVal(xListFromFile, current) for current in v]
+    # now we have list with zeros and ones, return true if and between them returns true. Otherwise false.
+    for i in zerosAndOnesList:
+        if i == 0:
+            return False # since 0 &&.... && = 0 always.
+    return True
+
+
+
+# helper function that gets current list and an integer (>0 or <0)
+# Returns his value in our expression (0 or 1)
+# for example if we got -3 = not(x3)
+# and if we got 4 = x4
+def convertToVal(xListFromFile, x):
+    if (x > 0):
+        toReturn = xListFromFile[x - 1]  # since the first value in list is for x1 and not x0.
+    else:
+        toReturn = (xListFromFile[-x - 1] + 1) % 2  # if 0 now it's 1, if 0 now it's 2.
+    return toReturn
+
+def main():
+    X, Y, d =  loadDataFromFile()
+    h = ConsistencyAlgorithm(X, Y, d, len(Y))
+    covertToText(h)
+
+
+
+# main function here..
+main()
---
+++ Python - Machine Learning/ex2.py
@@ -0,0 +1,112 @@
+# Ofir Ben Shoham.
+# 208642496.
+
+import numpy
+import matplotlib.pyplot as plt
+
+
+def main():
+    W, B  = train(generateTrainingSet(), 1000, 0.01, 0.01)
+    test = []
+    for x in numpy.linspace(0, 10, 100):
+        test.append(softMax(W, x, B)[0])
+    plotFunc(numpy.linspace(0, 10, 100),generateTrueDistibution(),test)
+
+
+
+# return tuple of 300 point..
+# Each point has (float number, class tag). class tag = 1/2/3.
+def generateTrainingSet():
+    Xvalues = []
+    Xvalues.extend(numpy.random.normal(2, 1, 100))
+    Xvalues.extend(numpy.random.normal(4, 1, 100))
+    Xvalues.extend(numpy.random.normal(6, 1, 100))
+    # now Xvalues contains 300 values.
+
+
+    Ytags = []
+    for classNum in range(1, 4):
+        Ytags.extend([classNum for i in range(100)])
+    # now Ytags contians {1..1 (100 times), 2...2 (100 times), and then 3..3 (100 times also).
+
+    # return list that each element is a tuple of a value and a class tag (1/2/3).
+    trainingSetToReturn = zip(Xvalues, Ytags)
+    return trainingSetToReturn
+
+# getting W, B, num (current float num), and a tag.
+# return the der -> dw, db.
+def gettingDer(W, B, num, tag, j):
+    updTag = tag - 1 # value between 0-3 as the indexes are.
+    smVal = softMax(W, num, B)
+    if updTag == j:
+        # it means i = j
+        dw = smVal * num - num
+        db = smVal - 1
+    else:
+        # it means i != j
+        dw = smVal * num
+        db = smVal
+    return dw, db
+
+
+
+# getting the training set which compose from points(number, tag), and then train it.
+def train(trainingSet, epochNumber, learningRate, bRate):
+    W = numpy.random.random((3, 1)) # initialized by random.
+    B = numpy.random.random((3, 1)) # the same.
+
+    for i in range(epochNumber):
+        numpy.random.shuffle(trainingSet)
+        for num,tag in trainingSet:
+            # into predictVec we have list that each element predict what is the probability that
+            # we into his class(element into index 0 = changes to be in class 1).
+
+
+            for inx in range(0, 3):
+                dw, db = gettingDer(W, B, num, tag, inx)
+                # update W and B:
+                W[inx] = W[inx] - learningRate * dw[inx]
+                B[inx] = B[inx] - bRate * db[inx]
+
+    return W, B
+
+
+def generateTrueDistibution():
+    finalTrueProb = []
+
+    for x in numpy.linspace(0, 10, 100):
+        temp = [normalCalc(x, currentMiu) for currentMiu in range(2, 8, 2)]
+        finalTrueProb.append(temp[0] / (temp[0] + temp[1] + temp[2]))
+
+    return finalTrueProb
+
+# function that returns the result of softmax function.
+"""
+W is a vector of K x 1, each row is a float number for class (since we have K calsses).
+X is the input vector -> size of D rows, it's a vector. 
+B is a vector.
+return a float number which is the result of the softmax (= the probability).
+"""
+def softMax(W, X, B):
+    x = W * X + B
+    e_x = numpy.exp(x - numpy.max(x))
+    return e_x / e_x.sum(axis=0)
+
+
+# comupte the prob of the normal distribution.
+def normalCalc(x, miu):
+    return 1 / numpy.sqrt(2 * numpy.pi) * numpy.exp((-(x - miu) ** 2) / 2)
+
+
+# plot the true distribution graph.
+def plotFunc(xToDraw, truthProb, trainedProb):
+    plt.xlabel('X')
+    plt.ylabel('p(y = 1 | x)')
+    plt.plot(xToDraw, truthProb, label='true distribution')
+    plt.plot(xToDraw, trainedProb, label='trained prob')
+    plt.legend()
+    plt.show()
+
+main()
+
+
